a7d8b150d133795b60b04b148f9dbd9f57ae6679
EileenFeng <efeng@brynmawr.edu>
2018-03-20 23:19:04 -0400

    done

--- test_fifo_create.c
+++ test_fifo_create.c
@@ -16,7 +16,7 @@ void foo(void* arg) {
 int main(void) {
   printf(" * Test for creating and running 100 threads (FIFO) \n");
   printf(" * Once joined the very first thread, the scheduler will automatically start scheduling and running threads from the HEAD of the read queue\n");
-  
+
   if (thread_libinit(FIFO) == -1)
     exit(EXIT_FAILURE);
 
@@ -25,9 +25,12 @@ int main(void) {
   for (int i = 0; i < N; i++)  {
     tids[i] = thread_create(foo, "FIFO", 1);
   }
+  for(int i = 0; i < N; i++){
+      if(thread_join(tids[i]) == -1){
+        exit(EXIT_FAILURE);
+      }
+  }
 
-  thread_join(tids[0]);
-  
   printf("back to main context in test_fifo_create\n");
   if (thread_libterminate() == -1)
     exit(EXIT_FAILURE);
